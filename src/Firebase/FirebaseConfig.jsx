1af19f699626c67a28466eb3788dc39b8e26ad5f
Swayam <swayamvij12@gmaial.com>
2023-12-09 14:46:03 +0530

    -

--- src/Firebase/FirebaseConfig.jsx
+++ src/Firebase/FirebaseConfig.jsx
@@ -6,16 +6,16 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FireBase,
+  apiKey: import.meta.env.VITE_FireBase_APIKEY,
   authDomain: "blogginssite.firebaseapp.com",
   projectId: "blogginssite",
   storageBucket: "blogginssite.appspot.com",
-  messagingSenderId: "32377880956",
-  appId: "1:32377880956:web:e132ea74ff1aea93f724f9",
-  measurementId: "G-Y2PVXQ92K5",
+  messagingSenderId: import.meta.env.VITE_FireBase_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FireBase_APPID,
+  measurementId: import.meta.env.VITE_MeasurementID,
 };
 
-// Initialize Firebase
+// Initialize Firebase:32377880956:web:e132ea74ff1aea93f724f9
 const app = initializeApp(firebaseConfig);
 const fireDB = getFirestore(app);
 const auth = getAuth(app);
